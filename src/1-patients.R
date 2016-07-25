21a57c41c877725095b88f9c2d49df23cd3d5552
bgulbis <brian.gulbis@gmail.com>
2016-07-25 15:42:37 -0500

    add script to generate patient encounter list for new data

---
+++ src/1-patients.R
@@ -0,0 +1,19 @@
+# 1-patients.R
+#
+# get list of patient encounters
+
+library(edwr)
+library(dplyr)
+library(lubridate)
+
+data.raw <- "../data/raw"
+
+# change the date to update for new patients
+dc.date <- "4/1/2016"
+
+raw.patients <- read_data(data.raw, "patients") %>%
+    as.patients() %>%
+    filter(age >= 18,
+           discharge.datetime >= mdy(dc.date, tz = "US/Central"))
+
+concat_encounters(raw.patients$pie.id)
