6bde8e2791095d9df2fc4a40c6a202e8abb434ca
Brian Gulbis <brian.gulbis@gmail.com>
2016-07-26 13:27:19 -0500

    use dirr package

--- setup.R
+++ setup.R
@@ -1,22 +1,4 @@
 # setup.R
 
-# create project directory structure
-
-make_dirs <- function(x) {
-    if (!dir.exists(x)) dir.create(x)
-}
-
-list_dirs <- list(
-    "data",
-    "data/raw",
-    "data/tidy",
-    "data/external",
-    "data/final",
-    "explore",
-    "figs",
-    "doc",
-    "report",
-    "src"
-)
-
-lapply(list_dirs, make_dirs)
+# compress data files
+dirr::gzip_files()
--- zip_files.R
+++
@@ -1,9 +0,0 @@
-# zip_files.R
-
-library(R.utils)
-
-data.raw <- "data/raw"
-
-comp.files <- list.files(data.raw, full.names = TRUE)
-
-lapply(comp.files, function(x) if (!isGzipped(x)) gzip(x, overwrite = TRUE))
